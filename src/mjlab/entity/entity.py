7423727a593c7ce5f536083283047ed60ba8dd62
Louis Le Lay <louislelay@pal-robotics.com>
2025-10-20 21:08:38 +0200

    saves progresses

--- src/mjlab/entity/entity.py
+++ src/mjlab/entity/entity.py
@@ -283,6 +283,8 @@ class Entity:
   ):
     if actuator_subset is None:
       actuator_subset = self.actuator_names
+    print(name_keys)
+    print(actuator_subset)
     return resolve_matching_names(name_keys, actuator_subset, preserve_order)
 
   def find_geoms(
