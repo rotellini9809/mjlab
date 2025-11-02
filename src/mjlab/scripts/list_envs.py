7dfea5c78b791fb15bbda8e7c7602c5006a9bde2
Louis Le Lay <louislelay@pal-robotics.com>
2025-11-02 01:39:46 +0100

    adds keyword argument

--- src/mjlab/scripts/list_envs.py
+++ src/mjlab/scripts/list_envs.py
@@ -1,12 +1,13 @@
 """Script to list MJLab environments."""
 
 import gymnasium as gym
+import tyro
 from prettytable import PrettyTable
 
 import mjlab.tasks  # noqa: F401 to register environments
 
 
-def main():
+def main(keyword: str | None = None):
   """Print all environments registered whose id contains `Mjlab-`."""
   prefix_substring = "Mjlab-"
 
@@ -19,7 +20,7 @@ def main():
   idx = 0
   for spec in gym.registry.values():
     try:
-      if prefix_substring in spec.id:
+      if prefix_substring in spec.id and (keyword is None or keyword in spec.id):
         env_cfg_ep = spec.kwargs.get("env_cfg_entry_point", "")
         table.add_row([idx + 1, spec.id, spec.entry_point, env_cfg_ep])
         idx += 1
@@ -28,12 +29,12 @@ def main():
 
   print(table)
   if idx == 0:
-    print(f"[INFO] No tasks matched filter: '{prefix_substring}'")
+    msg = f"[INFO] No tasks matched filter: '{prefix_substring}'"
+    if keyword:
+      msg += f" and keyword '{keyword}'"
+    print(msg)
   return idx
 
 
 if __name__ == "__main__":
-  try:
-    main()
-  except Exception as e:
-    raise RuntimeError(f"Error listing environments: {e}") from e
+  tyro.cli(main)
