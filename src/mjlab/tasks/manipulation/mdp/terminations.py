05beeb0ce102e09c9d863929d2962aaf7ce5f3ba
Kevin Zakka <kevinarmandzakka@gmail.com>
2025-12-01 14:03:17 -0800

    Remove no longer used termination in manipulation mdp.

--- src/mjlab/tasks/manipulation/mdp/terminations.py
+++ src/mjlab/tasks/manipulation/mdp/terminations.py
@@ -4,7 +4,6 @@ from typing import TYPE_CHECKING
 
 import torch
 
-from mjlab.entity import Entity
 from mjlab.sensor import ContactSensor
 
 if TYPE_CHECKING:
@@ -15,14 +14,3 @@ def illegal_contact(env: ManagerBasedRlEnv, sensor_name: str) -> torch.Tensor:
   sensor: ContactSensor = env.scene[sensor_name]
   assert sensor.data.found is not None
   return torch.any(sensor.data.found, dim=-1)
-
-
-def object_out_of_reach(
-  env: ManagerBasedRlEnv, object_name: str, max_distance: float
-) -> torch.Tensor:
-  robot: Entity = env.scene["robot"]
-  obj: Entity = env.scene[object_name]
-  robot_pos = robot.data.root_link_pos_w
-  obj_pos = obj.data.root_link_pos_w
-  horizontal_distance = torch.norm(obj_pos[:, :2] - robot_pos[:, :2], dim=-1)
-  return horizontal_distance > max_distance
